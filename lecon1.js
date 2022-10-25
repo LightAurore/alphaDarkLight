ca47a848c78ad6797f45a9a0bb711bd56f1bd275
LightAurore <115423063+LightAurore@users.noreply.github.com>
2022-10-26 00:11:09 +0200

    Add files via upload

---
+++ lecon1.js
@@ -0,0 +1,22 @@
+
+
+// Les String
+
+let firstName = "Will";
+let lastName = 'Alexander';
+
+// La concaténation
+let wholeName = firstName + " " + lastName;  // valeur: "Will Alexander"
+
+// La String Interpolation
+const myName = `Alexander`;
+const salutation = `Bienvenue sur mon site ${myName}!`;
+console.log(salutation);   //retournera “Bienvenue sur mon site Alexander!” 
+
+// Les Booleans
+let userIsSignedIn = true;
+let userIsAdmin = false;
+
+console.log(userIsAdmin);
+
+
